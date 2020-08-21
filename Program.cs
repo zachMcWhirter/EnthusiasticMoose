db8934b73be4289b5b7e7fb762c9e62860524500
Zach McWhirter <zmac7811@gmail.com>
2020-08-21 10:18:30 -0500

    finish phase 3

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EnthusiasticMoose
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
+            Console.WriteLine("--------------------------------------------");
+        }
+    }
+}
