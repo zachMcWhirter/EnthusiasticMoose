0a7520138d6919f038c798e4b0de5e12931fce18
Zach McWhirter <zmac7811@gmail.com>
2020-08-21 10:37:12 -0500

    do phase 5

--- Program.cs
+++ Program.cs
@@ -13,7 +13,32 @@ namespace EnthusiasticMoose
             // Let the moose speak!
             MooseSays("H I, I'M  E N T H U S I A S T I C !");
             MooseSays("I really am enthusiastic");
+
+            // As a question
+            bool isTrue = MooseAsks("Is Canada real?");
+            Console.WriteLine(isTrue);
         }
+        static bool MooseAsks(string question)
+        {
+            Console.Write($"{question} (Y/N): ");
+            string answer = Console.ReadLine().ToLower();
+
+            while (answer != "y" && answer != "n")
+            {
+                Console.Write($"{question} (Y/N): ");
+                answer = Console.ReadLine().ToLower();
+            }
+
+            if (answer == "y")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
         static void MooseSays(string message)
         {
             Console.WriteLine($@"
